b7a5468bb194b4dbb4a2bc28c62e2c8d260e7a57
kaup <kalebroyolson+github@gmail.com>
2023-09-30 18:39:48 +0000

    dynamo update

--- src/funcs/postdirtalert.go
+++ src/funcs/postdirtalert.go
@@ -1,22 +1,49 @@
 package main
 
 import (
-    "context"
-    "fmt"
-    "encoding/json"
-    "dirtie/src/common/models"
-    "github.com/aws/aws-lambda-go/lambda"
-    "github.com/aws/aws-lambda-go/events"
+	"context"
+	"dirtie/src/common/models"
+	"encoding/json"
+	"fmt"
+	"log"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
 )
 
 func HandleRequest(ctx context.Context, sqsEvent events.SQSEvent) error {
+    cfg, err := config.LoadDefaultConfig(ctx)
+    if err != nil {
+        return fmt.Errorf("error loading sdk config: %v\n", err)
+    }
+
+    client := dynamodb.NewFromConfig(cfg)
+    
+    errCount := 0
     for _, message := range sqsEvent.Records {
         fmt.Printf("Message: %s \n Event Source %s: %s \n", message.MessageId, message.EventSource, message.Body)
 
         var dirtAlert models.DirtAlert
         json.Unmarshal([]byte(message.Body), &dirtAlert) 
-    }
+        
+        putArgs := &dynamodb.PutItemInput{
+            TableName: aws.String("dirt_alerts"),
+        }
 
+        _, err = client.PutItem(ctx, putArgs)
+        if err != nil {
+            log.Printf("Error updating item: %v \n", err)
+            errCount++
+        }
+
+    }
+    
+    if errCount != 0 {
+        return fmt.Errorf("%v errors occurred, check cloudwatch for output", errCount)
+    }
     return nil
 }
 
